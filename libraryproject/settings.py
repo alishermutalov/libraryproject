06839f359f6bd2dbaec642619e5b90128a3ec334
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-09-17 20:29:19 +0500

    DB settings Updated

--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -95,10 +95,15 @@ WSGI_APPLICATION = 'libraryproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '<database-name>',
+        'USER': '<database-user>',
+        'PASSWORD': '<database-password>',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
